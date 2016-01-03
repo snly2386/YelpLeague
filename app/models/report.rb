8fe58f9b404342e18ac0c0916f6506b78f70ac68
Christopher Palmer <snly2386@gmail.com>
2016-01-02 21:49:47 -0800

    remove uniqueness

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,6 @@ class Report < ActiveRecord::Base
   belongs_to :user
   belongs_to :player
   validates_presence_of :message, :rating, :user_id, :player_id
-  validates_uniqueness_of :user_id
 
   def user_report_count
     user.report_count
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'landing#index'
+
+  devise_for :users
+
   resources :users, only: :show do
     resources :reports, only:[:index]
   end
