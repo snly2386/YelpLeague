c87334fbd5adee3c5c10e2f9feed170892983ed8
Wendy Hsu <wndyhsu@gmail.com>
2016-02-25 21:10:49 -0500

    changes on show page

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -8,19 +8,21 @@ $( document ).ready(function() {
 
   $("body").css("background-color", "black");
   $(".video-overlay").css("opacity", 0);
-  $("nav").css("opacity", 0);
+  $("#landing nav").css("opacity", 0);
   $("video").css("opacity", 0);
-  $("header").css("opacity", 0);
+  $("header.landing").css("opacity", 0);
+  $(".search_jobs").css("opacity", 0);
   $(".recent-searches").css("opacity", 0);
-  $("footer").css("opacity", 0);
+  $("#landing footer").css("opacity", 0);
 
   $(".assembly").transition({ opacity: 0, delay: 4000, easing: "out" }, 500);
   $(".video-overlay").transition({ opacity: 1, delay: 5000 }, 500);
-  $("nav").transition({ opacity: 1, delay: 6000 }, 500);
+  $("#landing nav").transition({ opacity: 1, delay: 6000 }, 500);
   $(".recent-searches").transition({ opacity: 1, delay: 7000 }, 500);
   $("footer").transition({ opacity: 1, delay: 7000 }, 500);
   $("video").transition({ opacity: 1, delay: 6000 }, 500);
-  $("header").transition({ opacity: 1, delay: 7000, easing: "in" }, 500);
+  $("header.landing").transition({ opacity: 1, delay: 7000, easing: "in" }, 500);
+  $(".search_jobs").transition({ opacity: 1, delay: 7000 }, 500);
   $("header h1").textillate({
     initialDelay: 3000,
     in: {
