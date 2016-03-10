feff1db9a842f5d768414d2ea953ad99fd308540
Christopher Palmer <snly2386@gmail.com>
2016-03-10 07:50:54 -0500

    remove scroll from module

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('yelpleague', ['templates', 'ngResource', 'yaru22.angular-timeago', 'ui.bootstrap', 'angularUtils.directives.dirPagination', 'duScroll']);
+var app = angular.module('yelpleague', ['templates', 'ngResource', 'yaru22.angular-timeago', 'ui.bootstrap', 'angularUtils.directives.dirPagination']);
